f44e95051f347ef37ccb5a3cc9fd695e1ef80cea
Curtis Ellis <curtis@nextcash.tech>
2020-04-16 19:55:11 -0600

    Fix fetch xpub error and add tests.

--- internal/oracle/oracle_test.go
+++ internal/oracle/oracle_test.go
@@ -45,4 +45,92 @@ func TestUsers(t *testing.T) {
 	if err := CreateUser(ctx, test.MasterDB, user); err != nil {
 		t.Fatalf("Failed to create user : %s", err)
 	}
+
+	fuser, err := FetchUser(ctx, test.MasterDB, user.ID)
+	if err != nil {
+		t.Fatalf("Failed to fetch user : %s", err)
+	}
+
+	if fuser.ID != user.ID {
+		t.Fatalf("Invalid user id")
+	}
+}
+
+func TestXPub(t *testing.T) {
+	ctx := tests.Context()
+	test := tests.New()
+
+	key, err := bitcoin.GenerateKey(bitcoin.MainNet)
+	if err != nil {
+		t.Fatalf("Failed to generate user key : %s", err)
+	}
+
+	entity := actions.EntityField{
+		Name:        "Test Entity Name",
+		CountryCode: "AUS",
+	}
+
+	entityBytes, err := proto.Marshal(&entity)
+	if err != nil {
+		t.Fatalf("Failed to serialize user entity : %s", err)
+	}
+
+	user := User{
+		ID:           uuid.New().String(),
+		Entity:       entityBytes,
+		PublicKey:    key.PublicKey(),
+		DateCreated:  time.Now(),
+		DateModified: time.Now(),
+		IsDeleted:    false,
+	}
+
+	if err := CreateUser(ctx, test.MasterDB, user); err != nil {
+		t.Fatalf("Failed to create user : %s", err)
+	}
+
+	xp, err := bitcoin.GenerateMasterExtendedKey()
+	if err != nil {
+		t.Fatalf("Failed to create xpub : %s", err)
+	}
+
+	xpubs := bitcoin.ExtendedKeys{xp}
+
+	xpub := XPub{
+		ID: uuid.New().String(),
+		UserID: user.ID,
+		XPub: xpubs,
+		RequiredSigners: 1,
+		DateCreated: time.Now(),
+	}
+
+	if err := CreateXPub(ctx, test.MasterDB, xpub); err != nil {
+		t.Fatalf("Failed to create Xpub : %s", err)
+	}
+
+	fxpub, err := FetchXPubByXPub(ctx, test.MasterDB, xpubs)
+	if err != nil {
+		t.Fatalf("Failed to fetch xpub : %s", err)
+	}
+
+	if !fxpub.XPub.Equal(xpubs) {
+		t.Fatalf("Invalid fetched xpubs")
+	}
+
+	userid, err := FetchUserIDByXPub(ctx, test.MasterDB, xpubs)
+	if err != nil {
+		t.Fatalf("Failed to fetch xpub user id : %s", err)
+	}
+
+	if userid != user.ID {
+		t.Fatalf("Invalid xpub user id")
+	}
+
+	fuser, err := FetchUserByXPub(ctx, test.MasterDB, xpubs)
+	if err != nil {
+		t.Fatalf("Failed to fetch user by xpubs : %s", err)
+	}
+
+	if fuser.ID != user.ID {
+		t.Fatalf("Invalid user id")
+	}
 }
