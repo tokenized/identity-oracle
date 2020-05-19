095a9b28cf4bfb9fece670d2566538fbf30579af
Curtis Ellis <curtis@nextcash.tech>
2020-05-19 14:34:57 -0600

    Remove code smell

--- conf/get_aws_env.py
+++ conf/get_aws_env.py
@@ -53,26 +53,27 @@ def load_variables():
             SecretId=secret_name
         )
     except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            raise e
-        elif e.response['Error']['Code'] == 'UnrecognizedClientException':
-            # Invalid AWS Key ID / Secret
-            raise e
-        else:
-            raise e
+        # if e.response['Error']['Code'] == 'DecryptionFailureException':
+        #     # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+        #     raise e
+        # elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+        #     # An error occurred on the server side.
+        #     raise e
+        # elif e.response['Error']['Code'] == 'InvalidParameterException':
+        #     # You provided an invalid value for a parameter.
+        #     raise e
+        # elif e.response['Error']['Code'] == 'InvalidRequestException':
+        #     # You provided a parameter value that is not valid for the current state of the resource.
+        #     raise e
+        # elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+        #     # We can't find the resource that you asked for.
+        #     raise e
+        # elif e.response['Error']['Code'] == 'UnrecognizedClientException':
+        #     # Invalid AWS Key ID / Secret
+        #     raise e
+        # else:
+        #     raise e
+        raise e
     else:
         # Decrypts secret using the associated KMS CMK.
         if 'SecretString' in get_secret_value_response:
